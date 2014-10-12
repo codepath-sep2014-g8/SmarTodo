59d333222c470be1c8df364d7bcd580dcd279277
dperiwal <dperiwal@gmail.com>
2014-10-12 15:44:49 -0700

    Consolidated Parse configuration code at one place.

--- SmarTodo/src/com/codepath/smartodo/ParseConfig.java
+++ SmarTodo/src/com/codepath/smartodo/ParseConfig.java
@@ -10,14 +10,23 @@ import com.parse.Parse;
 import com.parse.ParseObject;
 
 public class ParseConfig {
-	public static final String APPLICATION_ID = "q1Xy6yMRQSGUHU581wZ91YxspkdLwYbRb1CFy1fH";
-	public static final String CLIENT_KEY = "fWs7vfkXICxUmcaKuXdKLi9ZMYNGDzhQDDTuQoHB";
-
 	public static void init(Context context) {
-        ParseObject.registerSubclass(Address.class);
-        ParseObject.registerSubclass(User.class);
-        ParseObject.registerSubclass(TodoList.class);
-        ParseObject.registerSubclass(TodoItem.class);
-        Parse.initialize(context, ParseConfig.APPLICATION_ID, ParseConfig.CLIENT_KEY);
+		Parse.initialize(context, context.getString(R.string.parse_app_id),
+				context.getString(R.string.parse_client_key));
+		
+	    // Optional - If you don't want to allow Facebook login, you can
+	    // remove this line (and other related ParseFacebookUtils calls)
+	    // ParseFacebookUtils.initialize(getString(R.string.facebook_app_id));
+
+	    // Optional - If you don't want to allow Twitter login, you can
+	    // remove this line (and other related ParseTwitterUtils calls)
+	    //ParseTwitterUtils.initialize(getString(R.string.twitter_consumer_key),
+	    //   getString(R.string.twitter_consumer_secret));
+
+		Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
+		ParseObject.registerSubclass(Address.class);
+		ParseObject.registerSubclass(User.class);
+		ParseObject.registerSubclass(TodoList.class);
+		ParseObject.registerSubclass(TodoItem.class);
 	}
 }
--- SmarTodo/src/com/codepath/smartodo/activities/ListsViewerActivity.java
+++ SmarTodo/src/com/codepath/smartodo/activities/ListsViewerActivity.java
@@ -15,7 +15,6 @@ public class ListsViewerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	setContentView(R.layout.activity_lists_viewer);
-    	ParseConfig.init(this);
     }
 
     @Override

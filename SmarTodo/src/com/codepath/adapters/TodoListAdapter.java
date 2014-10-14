d4d979c7da02ea8ce35f98100edb72a2de149692
Praful Mantale <praful_mantale@yahoo.com>
2014-10-14 12:14:59 -0700

    Updated adapter

--- SmarTodo/src/com/codepath/adapters/TodoListAdapter.java
+++ SmarTodo/src/com/codepath/adapters/TodoListAdapter.java
@@ -8,6 +8,7 @@ import com.codepath.smartodo.model.TodoItem;
 import com.codepath.smartodo.model.TodoList;
 
 import android.content.Context;
+import android.net.ParseException;
 import android.text.Spannable;
 import android.text.Spanned;
 import android.text.style.StrikethroughSpan;
@@ -20,68 +21,74 @@ import android.widget.TextView;
 import com.etsy.android.grid.util.*;
 
 public class TodoListAdapter extends ArrayAdapter<TodoList> {
-	
+
 	private static final StrikethroughSpan STRIKE_THROUGH_SPAN = new StrikethroughSpan();
 
-	private class ViewHolder{
-	   DynamicHeightTextView txtItem;
-	   TextView txtTitle;
+	private class ViewHolder {
+		DynamicHeightTextView txtItem;
+		TextView txtTitle;
 	}
-	
-	public TodoListAdapter(Context context,  List<TodoList> objects) {
+
+	public TodoListAdapter(Context context, List<TodoList> objects) {
 		super(context, R.layout.item_todo_list, objects);
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		
+
 		TodoList todoList = getItem(position);
-		
+
 		ViewHolder viewHolder = null;
-		
-		if(convertView == null){
-			convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_todo_list, parent, false);
-			
-			viewHolder = new ViewHolder();			
-			viewHolder.txtItem = (DynamicHeightTextView)convertView.findViewById(R.id.txtItemList);
-			viewHolder.txtTitle = (TextView)convertView.findViewById(R.id.txtTitle);
-			
+
+		if (convertView == null) {
+			convertView = LayoutInflater.from(getContext()).inflate(
+					R.layout.item_todo_list, parent, false);
+
+			viewHolder = new ViewHolder();
+			viewHolder.txtItem = (DynamicHeightTextView) convertView
+					.findViewById(R.id.txtItemList);
+			viewHolder.txtTitle = (TextView) convertView
+					.findViewById(R.id.txtTitle);
+
 			convertView.setTag(viewHolder);
+		} else {
+			viewHolder = (ViewHolder) convertView.getTag();
 		}
-		else{
-			viewHolder = (ViewHolder)convertView.getTag();
-		}
-		
-		//convertView.setBackgroundColor(todoList.getColor);
-		
-		//get all the to do item
-		//iterate through them
-		//strike the one's which are completed
-		List<TodoItem> todoItems = new ArrayList<TodoItem>();// todoList.getTodoItems();
-		for(TodoItem item : todoItems){
-			String str = item.getText();
-			if(item.isCompleted()){
-				
-				//??? Check the logic.. does not work properly
-				int start = viewHolder.txtItem.getText().toString().length();
-				viewHolder.txtItem.append(str + "\r\n");
-				Spannable spannable = (Spannable) viewHolder.txtItem.getText();
-				spannable.setSpan(STRIKE_THROUGH_SPAN, start,
-						start + str.length() - 1,
-						Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+		// convertView.setBackgroundColor(todoList.getColor);
+
+		// get all the to do item
+		// iterate through them
+		// strike the one's which are completed
+		try {
+			List<TodoItem> todoItems = todoList.getAllItems();
+			for (TodoItem item : todoItems) {
+				String str = item.getText();
+				if (item.isCompleted()) {
+
+					// ??? Check the logic.. does not work properly
+					int start = viewHolder.txtItem.getText().toString()
+							.length();
+					viewHolder.txtItem.append(str + "\r\n");
+					Spannable spannable = (Spannable) viewHolder.txtItem
+							.getText();
+					spannable.setSpan(STRIKE_THROUGH_SPAN, start,
+							start + str.length() - 1,
+							Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+				} else {
+					viewHolder.txtItem.append(str + "\r\n");
+				}
+
 			}
-			else{
-				viewHolder.txtItem.append(str + "\r\n");
-			}
-			
+		} catch (com.parse.ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+
 		viewHolder.txtTitle.setText(todoList.getName());
-		
+
 		return convertView;
 	}
-	
-	
 
 }

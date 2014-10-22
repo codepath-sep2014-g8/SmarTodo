7dbaba96287e1eacba00c462636cdf30ab3880ef
Praful Mantale <praful_mantale@yahoo.com>
2014-10-22 16:55:23 -0700

    integrated fonts

---
+++ SmarTodo/src/com/codepath/smartodo/widgets/TypefacedTextView.java
@@ -0,0 +1,28 @@
+package com.codepath.smartodo.widgets;
+
+import com.codepath.smartodo.R;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class TypefacedTextView extends TextView {
+	public TypefacedTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        if(isInEditMode()){
+            return;
+        }
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TypefacedTextView);
+        String fontName = typedArray.getString(R.styleable.TypefacedTextView_typeface);
+        typedArray.recycle();
+
+        if(fontName != null){
+            Typeface typeface = Typeface.createFromAsset(context.getAssets(), fontName);
+            setTypeface(typeface);
+        }
+    }
+}

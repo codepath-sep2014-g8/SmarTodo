e62688e17ef9604057ff3f85f80b17d4a59aed27
Praful Mantale <praful_mantale@yahoo.com>
2014-10-24 15:33:40 -0700

    swipe listner

---
+++ SmarTodo/src/com/codepath/smartodo/listeners/OnSwipeTouchListener.java
@@ -0,0 +1,88 @@
+package com.codepath.smartodo.listeners;
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class OnSwipeTouchListener implements OnTouchListener{
+	
+	private GestureDetector gestureDetector;
+
+	
+	public OnSwipeTouchListener(Context context) {
+		gestureDetector = new GestureDetector(context, new GestureListener());
+	}
+
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		
+		return gestureDetector.onTouchEvent(event);
+	}
+	
+	private final class GestureListener extends SimpleOnGestureListener{
+			
+		 private static final int SWIPE_THRESHOLD = 100;
+	     private static final int SWIPE_VELOCITY_THRESHOLD = 100;
+		@Override
+		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+				float velocityY) {
+			
+			boolean result = false;
+			
+			try{
+				float diffY = e2.getY() - e1.getY();
+				float diffX = e2.getX() - e1.getX();
+				
+				//Horizontal swipe
+				if(Math.abs(diffX) > Math.abs(diffY)){
+					
+					if (Math.abs(diffX) > SWIPE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffX > 0) {
+                            onSwipeRight();
+                        } else {
+                            onSwipeLeft();
+                        }
+                    }
+				}
+				else{//Vertical swipe
+					if (Math.abs(diffY) > SWIPE_THRESHOLD && Math.abs(velocityY) > SWIPE_VELOCITY_THRESHOLD) {
+                        if (diffY > 0) {
+                            onSwipeDown();
+                        } else {
+                            onSwipeUp();
+                        }
+                    }
+				}
+			}
+			catch(Exception ex){
+				ex.printStackTrace();
+			}
+			
+			return result;
+		}
+		
+		
+		@Override
+		public boolean onDown(MotionEvent e) {
+		
+			return true;
+		}
+	     
+		public void onSwipeRight() {
+	    }
+	 
+	    public void onSwipeLeft() {
+	    }
+	 
+	    public void onSwipeUp() {
+	    }
+	 
+	    public void onSwipeDown() {
+	    }
+	     
+	}
+}

111f0d202b861ee66d16565ee9a15f13327fd36c
Damodar Periwal <dperiwal@gmail.com>
2014-11-15 23:21:52 -0800

    Introduced SmartTodoUser as a subclass of ParseUser.
    After reviewing and making any other changes to the new SmartTodoUser
    class, we should change all the references to the current User class to
    this new class. Perhaps, we can do things in the following order:
    
    1- Temporarily, remove this SmartTodoUser.java from the current
    codebase.
    2- Refactor the project by renaming the User class to SmartTodoUser
    class.
    3- Replace the old code of the just refactored SmartTodo.java with the
    new code set aside in step 1.
    4- Make other changes in the codebase to use the new SmartTodoUser class
    properly.

---
+++ SmarTodo/src/com/codepath/smartodo/model/SmarTodoUser.java
@@ -0,0 +1,126 @@
+package com.codepath.smartodo.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.util.Log;
+
+import com.parse.ParseClassName;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+@ParseClassName("_User")
+public class SmarTodoUser extends ParseUser {
+	private static final String GITHUB_USERNAME_KEY = "githubUsername";
+	private static final String PHONENUMBER_KEY = "phonenumber";
+	public static final String REALNAME_KEY = "realname";
+	public static final String OBJECTID_KEY = "objectId";
+	public static final String USERNAME_KEY = "username";
+	public static final String EMAIL_KEY = "email";
+
+	public SmarTodoUser() {
+		super();
+	}
+	
+	public List<TodoList> findAllLists() throws ParseException { // TODO: rename findAllRelevantTodoLists
+		ParseQuery<TodoList> todoListsQuery = ParseQuery.getQuery(TodoList.class);
+		todoListsQuery.whereEqualTo(TodoList.OWNER_KEY, this);
+		List<TodoList> relevantTodoLists = todoListsQuery.find();
+		
+		ParseQuery<TodoList> sharedTodoListsQuery = ParseQuery.getQuery(TodoList.class);
+		sharedTodoListsQuery.whereContainsAll(TodoList.SHARING_KEY, Arrays.asList(new SmarTodoUser[]{this})); // TODO: check this
+		relevantTodoLists.addAll(sharedTodoListsQuery.find());
+		
+		ParseObject.pinAll(relevantTodoLists);
+		
+		return relevantTodoLists;
+	}
+	
+	public static Collection<SmarTodoUser> findAll() {
+		return findAllLike(null, false);
+		
+	}
+	
+	public static Collection<SmarTodoUser> findAllLike(String substring) {
+		return findAllLike(substring, true);
+	}
+	
+	/**
+	 * Supports only substring based search in the realname, email and username fields. Note that the search is case SENSITIVE.
+	 * 
+	 * @param substring
+	 * @return
+	 */
+	public static Collection<SmarTodoUser> findAllLike(String substring, boolean doFilter) {
+		// TODO For some reason whereContains("*asdf*") does not work at all and returns 0 matches
+		// It may work now because of subclassing from the ParseUser class.
+		
+		String generousPattern = substring;//"*" + pattern + "*";
+		List<SmarTodoUser> users = new ArrayList<SmarTodoUser>();	
+		
+		ParseQuery<SmarTodoUser> smarTodoUserQuery;
+		try {
+			smarTodoUserQuery = ParseQuery.getQuery(SmarTodoUser.class);
+			if(doFilter) {
+				smarTodoUserQuery.whereContains(REALNAME_KEY, generousPattern);
+				smarTodoUserQuery.orderByAscending(OBJECTID_KEY);
+			}
+			users.addAll(smarTodoUserQuery.find());
+		} catch (ParseException e) {
+			Log.e("error", e.getMessage(), e);
+		}
+		
+		try {
+			smarTodoUserQuery = ParseQuery.getQuery(SmarTodoUser.class);
+			if(doFilter) {
+				smarTodoUserQuery.whereContains(EMAIL_KEY, generousPattern);
+				smarTodoUserQuery.orderByAscending(OBJECTID_KEY);
+			}
+			users.addAll(smarTodoUserQuery.find());
+		} catch (ParseException e) {
+			Log.e("error", e.getMessage(), e);
+		}
+		
+		try {
+			smarTodoUserQuery = ParseQuery.getQuery(SmarTodoUser.class);
+			if(doFilter) {
+				smarTodoUserQuery.whereContains(USERNAME_KEY, generousPattern);
+				smarTodoUserQuery.orderByAscending(OBJECTID_KEY);
+			}
+			users.addAll(smarTodoUserQuery.find());
+		} catch (ParseException e) {
+			Log.e("error", e.getMessage(), e);
+		}
+		
+		Map<String, SmarTodoUser> uniqueUsers = new HashMap<String, SmarTodoUser>();
+		
+		for(SmarTodoUser tmpUser : users) {
+			uniqueUsers.put(tmpUser.getUsername(), tmpUser);
+		}
+		
+		List<SmarTodoUser> filteredUsers = new ArrayList<SmarTodoUser>(uniqueUsers.values());
+		
+		try {
+			ParseObject.pinAll(filteredUsers);  // Doing this for local query later on? Where is that happening?
+		} catch (ParseException e) {
+			Log.e("error", e.getMessage(), e);
+		}
+		
+		return filteredUsers;
+	}
+	
+	public boolean equals(Object o) {
+		if(o instanceof SmarTodoUser) {
+			SmarTodoUser op = (SmarTodoUser) o;
+			return op.getEmail().equals(getEmail());
+		}
+		
+		return false;
+	}
+}
